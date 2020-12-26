5f3ee584f435bfde71564cc5d798a955a759a4ef
ysr075 <74384666+ysr075@users.noreply.github.com>
2020-12-26 19:39:48 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,18 +1,6 @@
 import sys
 print("\nWelcome to my calculator project on python\n\nprogrammed by @ysr075 on GitHub");
 while(True):
-    def add(x, y):
-        return x + y
-    
-    def substract(x, y):
-        return x - y
-    
-    def multiply(x, y):
-        return x * y
-    
-    def divide(x, y):
-        return x / y
-
     choose = input("\n1.add / 2.substract / 3.multiply / 4.divide: ")
     if choose == '1':
         num1 = float(input("\nnum1: "))
